168a1af99f07c7c15260a25259651dc49539e849
Jorge Avella <79177900+jaavella07@users.noreply.github.com>
2025-05-14 23:16:09 -0500

    paginationDto

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 
 @Injectable()
@@ -16,17 +17,25 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     return this.product.create({
       data: createProductDto
     })
-
-
   }
 
-  findAll() {
-    return this.product.findMany({})
+  findAll(paginationDto: PaginationDto) {
+
+    const { page = 0, limit = 0 } = paginationDto;
+
+
+    return this.product.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.product.findFirst({
+      where: { id }
+    })
   }
+
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
